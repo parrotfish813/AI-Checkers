670c4000174332f3374cae67e7e59f475ab0e291
parrotfish813 <bmt813@gmail.com>
2022-05-15 22:07:02 -0400

    Adding AI

--- checkers/game.py
+++ checkers/game.py
@@ -2,6 +2,7 @@ import pygame
 from .constants import RED, WHITE, BLUE, SQUARE_SIZE
 from checkers.board import Board
 
+
 class Game:
     def __init__(self, win):
         self._init()
@@ -18,13 +19,15 @@ class Game:
         self.turn = RED
         self.valid_moves = {}
 
+    def winner(self):
+        return self.board.winner()
+
     def reset(self):
         self._init()
 
     def select(self, row, col):
         if self.selected:
             result = self._move(row, col)
-
             if not result:
                 self.selected = None
                 self.select(row, col)
@@ -39,20 +42,22 @@ class Game:
 
     def _move(self, row, col):
         piece = self.board.get_piece(row, col)
-
         if self.selected and piece == 0 and (row, col) in self.valid_moves:
             self.board.move(self.selected, row, col)
             skipped = self.valid_moves[(row, col)]
-
             if skipped:
                 self.board.remove(skipped)
-
             self.change_turn()
-
         else:
             return False
 
         return True
+
+    def draw_valid_moves(self, moves):
+        for move in moves:
+            row, col = move
+            pygame.draw.circle(self.win, BLUE,
+                               (col * SQUARE_SIZE + SQUARE_SIZE // 2, row * SQUARE_SIZE + SQUARE_SIZE // 2), 15)
 
     def change_turn(self):
         self.valid_moves = {}
@@ -61,10 +66,9 @@ class Game:
         else:
             self.turn = RED
 
-    def draw_valid_moves(self, moves):
-        for move in moves:
-            row, col = move
-            pygame.draw.circle(self.win, BLUE, (col * SQUARE_SIZE + SQUARE_SIZE // 2, row * SQUARE_SIZE + SQUARE_SIZE // 2), 15)
+    def get_board(self):
+        return self.board
 
-    def winner(self):
-         return self.board.winner
+    def ai_move(self, board):
+        self.board = board
+        self.change_turn()
